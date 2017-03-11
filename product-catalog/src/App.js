e5fac5f52515ebe84703cbdced1a7d1d23fc6c4e
Anton Kovachev <anton.kovachev@amexis.net>
2017-03-11 17:43:27 +0200

    Small Refactoring

--- product-catalog/src/App.js
+++ product-catalog/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Collapsible from 'react-collapsible';
-
 import ProductCatalog from './components/productCatalog/ProductCatalog';
-
 
 import logo from './logo.svg';
 import './App.css';
